9fe485938b29e4e2824a13ca32da1b58a650889b
Namijamil <70440436+alnadzmerjamil@users.noreply.github.com>
2023-12-10 03:10:16 +0800

    fix getserverside props issue

--- app/project/page.tsx
+++ app/project/page.tsx
@@ -5,22 +5,17 @@ import MyProject from '@/components/Project';
 // data
 import constants from '@/constants';
 
-export async function getServerSideProps(ctx: any) {
-	const { params } = ctx;
+type Props = {
+	params: {};
+	searchParams: { [key: string]: string | string[] | undefined };
+};
 
-	return {
-		props: {
-			params,
-		},
-	};
-}
+function Project(props: Props) {
+	const searchParams = props.searchParams;
 
-function Project({
-	searchParams,
-}: {
-	searchParams?: { [key: string]: string };
-}) {
-	const name: string | undefined = searchParams?.name;
+	console.log('SEARCH PARAMS--->>>>', searchParams);
+
+	const name: string | string[] | undefined = searchParams?.name;
 
 	const data = name ? constants[name as keyof typeof constants] || {} : {};
 
